c54d8acc3877343266cdfa732094766fbde9fa32
Gianfranco D'Onofrio <gian7773@gmail.com>
2020-08-28 10:41:17 -0400

    ReactJs/NodeJs public heroku - V1.0.1

---
+++ public/precache-manifest.82ff8a1ef4412ab27862aa7e50414982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d44b5fa5b060396333b31ce1561925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "9cba62cdc4c5717d4479",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cba62cdc4c5717d4479",
+    "url": "/static/js/main.a7b53028.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
